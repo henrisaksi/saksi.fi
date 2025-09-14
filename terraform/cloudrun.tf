c52e920d5fc910c15e93cab14c5cc5ff89af54cc
henrisaksi <henri.saksi@fiksuruoka.fi>
2025-09-14 19:00:27 +0300

    cloud deploy owns deployments

--- terraform/cloudrun.tf
+++ terraform/cloudrun.tf
@@ -20,4 +20,12 @@ resource "google_cloud_run_service" "svelte_app" {
     google_artifact_registry_repository.app_repo,
     google_project_service.enabled_apis
   ]
+
+  lifecycle {
+    ignore_changes = [
+      template[0].spec[0].containers[0].image,
+      template[0].spec[0].service_account_name,
+      traffic,
+    ]
+  }
 }
